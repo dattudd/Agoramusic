02944f992dd597ae6ba43903d07a3e99ab76d827
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2023-12-31 16:27:47 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,61 +1,37 @@
-from typing import Union
-
 from pyrogram.types import InlineKeyboardButton
 
 import config
+from AnonXMusic import app
 
 
-
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌷𝐀ԃ𝐃 𝐌ҽ 𝐓σ 𝐘συ𝐑 𝐆ɾσυ𝐏🌷",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="𝐂σɱɱαɳԃ𝐒",
-                callback_data="settings_back_helper",
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(
-                text="𝐒ҽƚƚιɳɠ𝐒", callback_data="settings_helper"
-            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [
-            InlineKeyboardButton(
-                text="𝐅συɳԃҽ𝐑", user_id=OWNER),
-            InlineKeyboardButton(
-                text="𝐆ɾσυ𝐏", url=f"{config.SUPPORT_GROUP}"
-            ),
-        ],
-     ]
+    ]
     return buttons
 
 
-def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌷𝐀ԃ𝐃 𝐌ҽ 𝐓σ 𝐘συ𝐑 𝐆ɾσυ𝐏🌷",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            ),
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(
-                text="𝐂σɱɱαɳԃ𝐒", callback_data="settings_back_helper"
-            ),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
-        [
-            InlineKeyboardButton(text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER),
-            InlineKeyboardButton(
-                text="𝐆ɾσυ𝐏", url=f"{config.SUPPORT_GROUP}"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                    text="🌹 𝑀𝑦𝑟𝑎 network 🌹", url="https://t.me/myra_updates"
-                )
-        ],
-     ]
+    ]
+    return buttons
